f2caed77d97c217707437d4382160868bb06acd6
mahimerror <mahimislam46@gmail.com>
2025-05-13 11:37:22 +0600

    add tab

--- src/pages/ClientPage.jsx
+++ src/pages/ClientPage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Button, Tabs } from "antd";
+import { PlusIcon } from "./../assets/icons/icons";
+import { useState } from "react";
+
+const items = [
+  {
+    key: "1",
+    label: "All Clients",
+  },
+  {
+    key: "2",
+    label: "VIP Clients",
+  },
+  {
+    key: "3",
+    label: "Blacklisted Clients",
+  },
+];
 
 function ClientPage() {
+  const [tabKey, setTabKey] = useState("1");
+
+  const onChange = (key) => {
+    console.log(typeof key);
+    setTabKey(key);
+  };
+
   return (
-    <div>ClientPage</div>
-  )
+    <div className="w-full p-5">
+      <div className="flex items-center justify-between mb-4">
+        <Tabs
+          defaultActiveKey="1"
+          items={items}
+          onChange={onChange}
+          className="custom-client-tabs"
+        />
+        <button
+          type="button"
+          className="inline-flex items-center px-3 py-2 gap-2 text-sm font-semibold text-white bg-primary01 border border-primary01 rounded-lg hover:bg-primary01 focus:outline-none focus:ring-2 focus:ring-primary01 focus:ring-offset-2"
+        >
+          <PlusIcon />
+          Add Client
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default ClientPage
+export default ClientPage;

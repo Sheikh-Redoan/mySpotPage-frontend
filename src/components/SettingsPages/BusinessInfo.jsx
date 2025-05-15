05a52e5deec6800cb8802047b2090b58971b8b44
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-05-15 17:16:15 +0600

    Complete BusinessInfo page

--- src/components/SettingsPages/BusinessInfo.jsx
+++ src/components/SettingsPages/BusinessInfo.jsx
@@ -1,19 +1,93 @@
-import { ChevronDown } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { imageProvider } from "../../lib/imageProvider";
 import { CopyIcon } from "../../assets/icons/icons2";
+import { ConfigProvider } from "antd";
+import SearchableDropdown from "../ui/SearchableDropdown";
+import Dropdown from "../ui/Dropdown";
+import { useRef, useState } from "react";
+import { X } from "lucide-react";
+import AvatarEditor from "react-avatar-editor";
+
+const serviceOptions = [
+  { value: "nails", label: "Nails" },
+  { value: "hair_barber", label: "Hair & Barber" },
+  { value: "makeup", label: "Makeup" },
+  { value: "lash_brow", label: "Lash & Brow" },
+  { value: "waxing", label: "Waxing" },
+  { value: "tanning", label: "Tanning" },
+  { value: "massage", label: "Massage" },
+  { value: "skincare", label: "Skincare" },
+  { value: "spa_wellness", label: "Spas & Wellness" },
+  { value: "fitness", label: "Fitness" },
+  { value: "tattoo_piercing", label: "Tattoo & Piercing" },
+  { value: "teeth_white", label: "Teeth White" },
+  { value: "holistic", label: "Holistic" },
+];
+
+const businessOptions = [
+  {
+    value: "exempt_business_(Osek Patur)",
+    label: "Exempt Business (Osek Patur)",
+  },
+  {
+    value: "licensed_business_(Osek Murshé)",
+    label: "Licensed Business (Osek Murshé)",
+  },
+  {
+    value: "limited_company_(Ltd.)",
+    label: "Limited Company (Ltd.)",
+  },
+  {
+    value: "non_profit_organization",
+    label: "Non-Profit Organization",
+  },
+];
 
 const BusinessInfo = () => {
+  const [image, setImage] = useState(null);
+  const [editorImage, setEditorImage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { register, handleSubmit } = useForm();
+  const editorRef = useRef(null);
 
   const onSubmit = (data) => {
     console.log(data);
   };
+
+  const handleChange = (value) => {
+    console.log("Selected:", value);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageURL = URL.createObjectURL(file);
+      setEditorImage(imageURL);
+      setImage(imageURL);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+  };
+
+  const [imgSclaeValue, setImgSclaeValue] = useState(1.2);
+
+  const handleCropFinish = () => {
+    if (editorRef.current) {
+      const canvas = editorRef.current.getImageScaledToCanvas();
+      const croppedImage = canvas.toDataURL();
+
+      setImage(croppedImage);
+      setIsModalOpen(false);
+    }
+  };
+
   return (
     <div className="min-h-full">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="p-[26px] border-[1px] border-[#E7E7E7] bg-[#FFFFFF] rounded-md mt-4"
+        className="p-[26px] border-[1.5px] border-[#E7E7E7] bg-[#FFFFFF] rounded-md mt-4"
       >
         <div className="flex justify-between items-center my-4">
           <h2 className="text-[#262626] text-xl font-semibold">
@@ -29,24 +103,43 @@ const BusinessInfo = () => {
             Thumbnail <span className="text-orange-600">*</span>
           </label>
           <div className="mb-6 flex">
-            <label
-              htmlFor="thumbnail"
-              className="w-full max-w-md flex flex-col items-center justify-center border-2 border-dashed border-gray-400 p-8 rounded-lg cursor-pointer hover:border-[#866BE7] transition-all text-center"
-            >
-              <div className="flex flex-col items-center">
-                <img src={imageProvider.imageUploader} alt="Image" />
-                <p className="text-gray-600 text-lg font-semibold underline my-2">
-                  Upload Image
-                </p>
-                <p className="text-sm text-gray-500">JPEG, PNG up to 50MB</p>
+            {!image ? (
+              <label
+                htmlFor="thumbnail"
+                className="w-full max-w-md flex flex-col items-center justify-center border-2 border-dashed border-gray-400 p-8 rounded-lg cursor-pointer hover:border-[#866BE7] transition-all text-center"
+              >
+                <div className="flex flex-col items-center">
+                  <img src={imageProvider.imageUploader} alt="Image" />
+                  <p className="text-gray-600 text-lg font-semibold underline my-2">
+                    Upload Image
+                  </p>
+                  <p className="text-sm text-gray-500">JPEG, PNG up to 50MB</p>
+                </div>
+                <input
+                  id="thumbnail"
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleFileChange}
+                />
+              </label>
+            ) : (
+              <div className="relative w-full max-w-md">
+                <img
+                  onClick={() => setIsModalOpen(true)}
+                  src={image}
+                  alt="Uploaded"
+                  className="w-full h-[180px] rounded-lg object-cover"
+                />
+                <button
+                  type="button"
+                  onClick={handleRemoveImage}
+                  className="absolute -top-2 -right-2 bg-white border border-gray-300 rounded-full p-1 hover:bg-[#866BE7] hover:border-none transform transition-all duration-300 ease-in-out "
+                >
+                  <X className="w-5 h-5 text-[#866BE7] hover:text-[#FFFFFF]" />
+                </button>
               </div>
-              <input
-                id="thumbnail"
-                type="file"
-                accept="image/*"
-                className="hidden"
-              />
-            </label>
+            )}
           </div>
         </div>
 
@@ -90,38 +183,52 @@ const BusinessInfo = () => {
             <label className="block mb-2 text-[#888888]">
               Business Type <span className="text-orange-600">*</span>
             </label>
-            <select
-              {...register("businessType", { required: true })}
-              className="block text-sm w-full border border-gray-300 rounded-md p-2 appearance-none text-[#242528]"
-              required
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: "#8B5CF6",
+                  borderRadius: 8,
+                },
+                components: {
+                  Select: {
+                    controlHeight: 38,
+                  },
+                },
+              }}
             >
-              <option value="">Select Business Type</option>
-              <option value="service">Service</option>
-              <option value="product">Product</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 top-8 right-3 flex items-center text-[#888888]">
-              <ChevronDown />
-            </div>
+              <Dropdown
+                options={businessOptions}
+                placeholder="Select"
+                onChange={handleChange}
+              />
+            </ConfigProvider>
           </div>
 
           {/* Business Classification */}
-          <div className="flex-1 relative">
+          <div className="flex-1">
             <label className="block mb-2 text-[#888888]">
               Business Classification <span className="text-orange-600">*</span>
             </label>
-            <select
-              {...register("businessClassification", { required: true })}
-              className="block text-sm w-full border border-gray-300 rounded-md p-2 appearance-none text-[#242528]"
-              required
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: "#8B5CF6",
+                  borderRadius: 8,
+                },
+                components: {
+                  Select: {
+                    controlHeight: 38,
+                  },
+                },
+              }}
             >
-              <option value="">Select Classification</option>
-              <option value="retail">Retail</option>
-              <option value="wholesale">Wholesale</option>
-              <option value="franchise">Franchise</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 top-8 right-3 flex items-center text-[#888888]">
-              <ChevronDown />
-            </div>
+              <SearchableDropdown
+                options={serviceOptions}
+                placeholder="Select"
+                searchPlaceholder="Search"
+                onChange={handleChange}
+              />
+            </ConfigProvider>
           </div>
         </div>
 
@@ -169,6 +276,59 @@ const BusinessInfo = () => {
           />
         </div>
       </form>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 bg-[#111113cc] flex items-center justify-center">
+          <div className="bg-white rounded-lg shadow-lg w-[90%] max-w-3xl">
+            {/* Header */}
+            <div className="flex justify-between items-center py-6 px-6">
+              <h3 className="text-xl font-semibold text-[#242528]">
+                Crop image
+              </h3>
+              <button onClick={() => setIsModalOpen(false)}>
+                <X className="w-6 h-6 text-[#52555B] hover:scale-105 hover:text-[#3c3e42]" />
+              </button>
+            </div>
+
+            {/* Avatar Editor */}
+            <div className="flex justify-center items-center py-2">
+              <div
+                onWheel={(e) => {
+                  e.preventDefault();
+                  const delta = e.deltaY;
+
+                  setImgSclaeValue((prev) => {
+                    let newScale = prev + (delta > 0 ? -0.05 : 0.05);
+                    newScale = Math.min(Math.max(newScale, 0.5), 3);
+                    return newScale;
+                  });
+                }}
+              >
+                <AvatarEditor
+                  ref={editorRef}
+                  image={editorImage}
+                  width={720}
+                  height={450}
+                  scale={imgSclaeValue}
+                  rotate={0}
+                  className="object-contain w-full max-h-[70vh]"
+                />
+              </div>
+            </div>
+
+            {/* Finish Button */}
+            <div className="flex justify-end my-3 pr-6">
+              <button
+                onClick={handleCropFinish}
+                className="text-[#242528] cursor-pointer hover:text-[#141516] hover:font-semibold font-medium text-lg py-2"
+              >
+                Finish
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

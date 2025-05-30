3fe9798291efb1b42873774abac0545697a7586d
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-31 01:43:33 +0600

    feat: Implement pagination in UserManagement and add Paginate component

--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -1,16 +1,15 @@
 import { ListFilter } from "lucide-react";
-import { useState } from "react";
 import UserCard from "../../components/admin/UserCard";
 import UserFilter from "../../components/admin/UserFilter";
 import Filter from "../../components/shared/Filter";
+import Paginate from "../../components/shared/Paginate";
 import Popup from "../../components/shared/Popup";
 import Search from "../../components/shared/Search";
 
 export default function UserManagement() {
-  const [open, setOpen] = useState(true);
   return (
-    <div className="w-full p-5">
-      <div className="flex w-full gap-4 mb-4">
+    <div>
+      <div className="flex w-full gap-4 mb-4 p-5">
         <div className="relative w-[300px]">
           <Search name="search" placeholder="Search by name, or phone number" />
         </div>
@@ -31,10 +30,21 @@ export default function UserManagement() {
         </div>
       </div>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
+      </div>
+
+      <div className="fixed bottom-0 bg-white p-5 inset-x-0 pl-28">
+        <div className="flex items-center justify-between w-full">
+          <p className="space-x-2 text-sm">
+            Show <span className="px-4 py-2 bg-gray-100 rounded-lg">10</span> /
+            25 results per page
+          </p>
+
+          <Paginate total={500} current={4} />
+        </div>
       </div>
     </div>
   );

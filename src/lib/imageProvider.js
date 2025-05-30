f65a1d27ea43e573810e9f1af6567a72d3ed75eb
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-30 06:26:36 +0600

    refactor: Clean up image imports in imageProvider.js for consistency

--- src/lib/imageProvider.js
+++ src/lib/imageProvider.js
@@ -6,14 +6,13 @@ import CalendarPink from "../assets/icons/calendar-dots.png";
 import CalendarWhite from "../assets/icons/calenderWhite.png";
 import ChartLinePink from "../assets/icons/ChartLine.png";
 import ChartLineWhite from "../assets/icons/chartLineWhite.png";
-import databaseCol from "../assets/icons/colDatabase.png";
+import colDatabase from "../assets/icons/colDatabase.png";
 import colorPeople from "../assets/icons/colorPeople.png";
 import database from "../assets/icons/database.png";
 import deleteIcon from "../assets/icons/delete.png";
 import dollor from "../assets/icons/dollor.png";
 import edit from "../assets/icons/edit.png";
 import emtyBilling from "../assets/icons/emtyBilling.png";
-import filterIcon from "../assets/icons/filter.png";
 import glow from "../assets/icons/glow.png";
 import ServicePink from "../assets/icons/hand-heart.png";
 import imageUploader from "../assets/icons/imageUploader.png";
@@ -32,6 +31,8 @@ import PricingPink from "../assets/icons/Pricing-vertor.png";
 import PricingWhite from "../assets/icons/pricingWhite.png";
 import RevenueIcon from "../assets/icons/revenue.png";
 import SalesIcon from "../assets/icons/sales.png";
+import seller_pink from "../assets/icons/Seller.png";
+import seller_white from "../assets/icons/Seller_white.png";
 import ServiceWhite from "../assets/icons/serviceWhite.png";
 import SettingsPink from "../assets/icons/settings-vector.png";
 import SettingsWhite from "../assets/icons/settingsWhilte.png";
@@ -40,7 +41,6 @@ import subscription from "../assets/icons/subscription.png";
 import subscriptionAlert from "../assets/icons/subscriptionAlert.png";
 import upgradeIcon from "../assets/icons/upgradeIcon.png";
 import upload from "../assets/icons/upload.png";
-import user from "../assets/icons/user.png";
 import visa from "../assets/icons/visa.png";
 import TextLogo from "../assets/images/my_spot_page_text_favicon.png";
 
@@ -88,8 +88,6 @@ export const imageProvider = {
   visa,
   amrican,
   dollor,
-  user,
   database,
-  databaseCol,
-  filterIcon,
+  colDatabase,
 };

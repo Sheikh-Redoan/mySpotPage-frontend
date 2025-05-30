29d291731896acef13e45882a494423c580e2c21
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-30 23:28:12 +0600

    refactor: Update UserFilter and Popup components for improved layout and functionality

--- src/components/shared/Popup.jsx
+++ src/components/shared/Popup.jsx
@@ -2,7 +2,7 @@ import { Button } from "antd";
 import { X } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
-export default function Popup({ name, icon, children }) {
+export default function Popup({ name, icon, buttonComp, children }) {
   const [toggle, setToggle] = useState(true);
   const popupRef = useRef(null);
   const iconRef = useRef(null);
@@ -31,11 +31,19 @@ export default function Popup({ name, icon, children }) {
 
   return (
     <div className="relative">
-      <Button type="default" onClick={handlePopup} variant="outlined">
-        {icon && icon}
+      {name && (
+        <Button type="default" onClick={handlePopup} variant="outlined">
+          {icon && icon}
 
-        {name}
-      </Button>
+          {name}
+        </Button>
+      )}
+
+      {buttonComp && (
+        <Button type="default" onClick={handlePopup} variant="outlined">
+          {buttonComp()}
+        </Button>
+      )}
 
       <div
         ref={popupRef}
@@ -44,12 +52,14 @@ export default function Popup({ name, icon, children }) {
             ? "scale-0 opacity-0 invisible"
             : "scale-100 opacity-100 visible"
         } w-[350px] transition-all duration-300  absolute top-0  z-[10] left-1/2 rounded-xl bg-white shadow-lg inset-shadow-sm`}>
-        <div className="flex justify-between items-center border-b border-b-gray-100 px-3 py-2">
-          <h3 className="text-lg font-semibold">{name}</h3>
-          <Button type="text" onClick={handlePopup} variant="outlined">
-            <X size={20} strokeWidth={1} className="text-gray-600" />
-          </Button>
-        </div>
+        {name && (
+          <div className="flex justify-between items-center border-b border-b-gray-100 px-3 py-2">
+            <h3 className="text-lg font-semibold">{name}</h3>
+            <Button type="text" onClick={handlePopup} variant="outlined">
+              <X size={20} strokeWidth={1} className="text-gray-600" />
+            </Button>
+          </div>
+        )}
 
         <div className="flex items-center gap-2 p-4">{children}</div>
       </div>

62d9fa75449902111e81f2b4bf51cb2947ad4aa2
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-04 06:25:08 +0600

    feat: enhance BookingDetailsContent with new button for blacklist addition, improved layout, and updated date selection component

--- src/components/calendarManagement/pendingBookings/BookingDetailsContent.jsx
+++ src/components/calendarManagement/pendingBookings/BookingDetailsContent.jsx
@@ -1,4 +1,4 @@
-import { Select } from "antd";
+import { Button, Select } from "antd";
 import { CircleUserRound } from "lucide-react";
 import { PhoneCall } from "lucide-react";
 import { Crown } from "lucide-react";
@@ -7,9 +7,12 @@ import "react-datepicker/dist/react-datepicker.css";
 import { Link } from "react-router";
 import StaffReassignSelect from "./StaffReassignSelect";
 import { Calendar } from "lucide-react";
+import { CircleAlert } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 const BookingDetailsContent = ({ selectedDate, setSelectedDate, booking }) => {
   console.log("booking", booking);
+
   const formatDate = (date) => {
     return new Intl.DateTimeFormat("en-US", {
       weekday: "short",
@@ -27,7 +30,7 @@ const BookingDetailsContent = ({ selectedDate, setSelectedDate, booking }) => {
   return (
     <div className="w-full flex flex-col lg:flex-row">
       {/* Left Section: Client and Booking Info */}
-      <div className="flex-1 border-r-[1px] border-t-[1px] border-t-gray-300 border-r-gray-300 p-4">
+      <div className="border-r-[1px] border-t-[1px] border-t-gray-300 border-r-gray-300 p-4">
         <div className="space-y-4 border-b-[1px] border-b-gray-300 pb-3">
           <div className="w-16 h-16 bg-primary01 text-white flex items-center justify-center rounded-full text-xl font-bold">
             {booking.avatar}
@@ -45,7 +48,7 @@ const BookingDetailsContent = ({ selectedDate, setSelectedDate, booking }) => {
           </div>
         </div>
 
-        <div className="py-3 space-y-3 border-b-[1px] border-dashed border-b-gray-300">
+        <div className="py-4 space-y-3 border-b-[2px] border-dashed border-b-gray-300">
           <div className="flex items-center gap-3">
             <PhoneCall className="size-5 text-gray-600" />
             <p className="text-[#242528] text-[14px] font-normal">
@@ -75,56 +78,52 @@ const BookingDetailsContent = ({ selectedDate, setSelectedDate, booking }) => {
             cut with light texture.
           </p>
         </div>
-      </div>
 
-      {/* Middle Section: Services INfo */}
-      <div className="flex-1 border-r-[1px] border-t-[1px] border-t-gray-300 border-r-gray-300 p-4">
-        <div className="space-y-4 border-b-[1px] border-b-gray-300 pb-3">
-          <div className="w-16 h-16 bg-primary01 text-white flex items-center justify-center rounded-full text-xl font-bold">
-            {booking.avatar}
-          </div>
-          <div className="flex items-center gap-2">
-            <Link
-              to={"#"}
-              className="text-[#3E70DD] underline decoration-1 decoration-Boulder-400 text-[20px] font-medium"
-            >
-              {booking.clientName}
-            </Link>
-            <div className="w-5 h-5 rounded-full bg-[#FFB743] flex items-center justify-center">
-              <Crown className="text-white size-3" />
-            </div>
+        <div className="py-4">
+          <p className="text-[#3A3B3F] text-[14px] font-normal">
+            Specific notes
+          </p>
+          <div className="border-[1px] border-[#E5E7E8] py-2 px-3 h-44 rounded-lg shadow-sm">
+            <p className="text-[#424348] text-sm font-normal p-2">
+              Shop recommend booking a trim every 6-8 weeks
+            </p>
           </div>
         </div>
 
-        <div className="py-3 space-y-3">
-          <div className="flex items-center gap-3">
-            <PhoneCall className="size-5 text-gray-600" />
-            <p className="text-[#242528] text-[14px] font-normal">
-              {booking.clientPhone}
-            </p>
-          </div>
+        <div>
+          <Button color="danger" variant="text" className="flex items-center">
+            <CircleAlert className="size-5" />
+            Add to blacklist
+          </Button>
+        </div>
+      </div>
 
-          <div className="flex items-center gap-3">
-            <div className="flex items-center gap-3">
-              <CircleUserRound className="size-5 text-gray-600" />
-              <p className="text-[#242528] text-[14px] font-normal">Staff - </p>
-            </div>
-            <StaffReassignSelect
-              booking={booking}
-              onReassign={handleReassign}
+      {/* Middle Section: Services INfo */}
+      <div className="flex-1 border-r-[1px] border-t-[1px] border-t-gray-300 border-r-gray-300">
+        <div className="bg-[#F5F4FE] w-full p-4 flex justify-between items-center">
+          <div className="relative">
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="EEE, dd MMM yyyy"
+              customInput={
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <span className="text-lg font-semibold text-[#6C5DD3]">
+                    {formatDate(selectedDate)}
+                  </span>
+                  <ChevronDown className="size-5 text-[#6C5DD3]" />
+                </div>
+              }
             />
           </div>
-
-          <div className="flex items-center gap-3">
-            <Calendar className="size-5 text-gray-600" />
-            <p className="text-[#242528] text-sm">{booking.scheduledDate},</p>
-            <p className="text-[#242528] text-sm">{booking.scheduledTime}</p>
+          <div className="flex items-center gap-2">
+            <span className="text-base text-[#242528] font-medium">
+              {booking.scheduledTime}
+            </span>
+            <div className="px-3 py-1 bg-[#E0E7FF] text-[#3E70DD] rounded-full text-sm font-medium">
+              Confirmed
+            </div>
           </div>
-
-          <p className="text-[#242528] text-sm font-noramal p-2">
-            Hair is thick and slightly wavy, prefers a shoulder-length layered
-            cut with light texture.
-          </p>
         </div>
       </div>
 

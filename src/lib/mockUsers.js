d5f9ae830ff45f2e41cc1f16d8145dff97423514
Rashad-Stack <tex.eng.rasehd@gmail.com>
2025-06-13 04:08:50 +0600

    feat: implement user authentication flow with Redux and enhance user management features

---
+++ src/lib/mockUsers.js
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    name: "Emma Johnson",
+    phone: "1",
+    role: "client",
+  },
+  {
+    name: "Liam Smith",
+    phone: "2",
+    role: "admin",
+  },
+  {
+    name: "Olivia Brown",
+    phone: "3",
+    role: "seller",
+  },
+];
+
+// {
+//   name: "User",
+//   role: "client",
+//   photoURL: null,
+// }
--- src/pages/authentication/Signin.jsx
+++ src/pages/authentication/Signin.jsx
@@ -1,32 +1,54 @@
 import { slideInFromRight } from "@/animations/variants";
 import { ArrowLeft } from "lucide-react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import { Link } from "react-router";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import { users } from "../../lib/mockUsers";
+import { setUser } from "../../redux/features/userSlice";
 
 const Signin = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted");
+    console.log("Phone:", e.target.phone.value);
+
+    const user = users.find((user) => user.phone === e.target.phone.value);
+
+    if (user) {
+      console.log("User found:", user);
+      dispatch(setUser(user));
+      navigate("/"); // Redirect to home page after successful login
+    } else {
+      console.log("User not found");
+    }
+  };
 
   return (
     <div className=" bg-gray-100 font-golos">
       <div className="sm:hidden flex items-center gap-2 px-4 pt-6">
         <Link
           to="/auth"
-          className="flex items-center text-[#0f0528] font-medium hover:underline"
-        >
+          className="flex items-center text-[#0f0528] font-medium hover:underline">
           <ArrowLeft size={20} />
           <span className="ml-2">Back</span>
         </Link>
       </div>
       <div className="flex justify-center items-center min-h-screen">
         <motion.form
+          onSubmit={handleSubmit}
           variants={slideInFromRight()}
           initial="hidden"
           animate="visible"
-          className="bg-white p-[40px] rounded-[12px] mx-3 sm:mx-0 shadow-md w-full max-w-md"
-        >
+          className="bg-white p-[40px] rounded-[12px] mx-3 sm:mx-0 shadow-md w-full max-w-md">
           <div className="flex items-center mb-6">
             <Link className="hidden sm:block" to={"/auth"}>
               <ArrowLeft size={24} />
@@ -40,8 +62,7 @@ const Signin = () => {
           <div className="mb-4">
             <label
               htmlFor="phone"
-              className="block text-gray-700 font-medium mb-1"
-            >
+              className="block text-gray-700 font-medium mb-1">
               Phone Number <span className="text-orange-600">*</span>
             </label>
             <input
@@ -56,8 +77,7 @@ const Signin = () => {
           <div className="mb-4">
             <label
               htmlFor="password"
-              className="block text-gray-700 font-medium mb-1"
-            >
+              className="block text-gray-700 font-medium mb-1">
               Password <span className="text-orange-600">*</span>
             </label>
             <div className="relative">
@@ -69,8 +89,7 @@ const Signin = () => {
               />
               <span
                 className="absolute right-3 top-3 text-gray-500 cursor-pointer"
-                onClick={() => setShowPassword(!showPassword)}
-              >
+                onClick={() => setShowPassword(!showPassword)}>
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </span>
             </div>
@@ -78,7 +97,6 @@ const Signin = () => {
 
           {/* Remember Me & Forgot Password */}
           <div className="flex items-center justify-between mb-6 text-sm">
-            {" "}
             {/* Added text-sm here */}
             <label className="flex items-center gap-2 text-gray-700">
               <input type="checkbox" className="accent-[#744CDB]" />
@@ -87,8 +105,7 @@ const Signin = () => {
             <Link to={"/forgot-password"}>
               <button
                 type="button"
-                className="text-[#744CDB] font-medium hover:underline"
-              >
+                className="text-[#744CDB] font-medium hover:underline">
                 Forgot Password?
               </button>
             </Link>
@@ -97,8 +114,7 @@ const Signin = () => {
           {/* Signin Button */}
           <button
             type="submit"
-            className="w-full bg-[#744CDB] text-white py-2 rounded-md hover:bg-[#633CDB] hover:scale-x-95 transition-all transform duration-200 text-sm"
-          >
+            className="w-full bg-[#744CDB] text-white py-2 rounded-md hover:bg-[#633CDB] hover:scale-x-95 transition-all transform duration-200 text-sm">
             Sign in
           </button>
 
@@ -108,8 +124,7 @@ const Signin = () => {
             <Link to={"/signup"}>
               <a
                 href="#"
-                className="text-[#744CDB] font-medium hover:underline"
-              >
+                className="text-[#744CDB] font-medium hover:underline">
                 Signup
               </a>
             </Link>

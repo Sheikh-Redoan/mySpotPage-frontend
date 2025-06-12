641a504b4590f19dd71fc7f41500f93736d7a718
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-13 04:15:36 +0600

    feat: add AntdCropImgUpload component and integrate it into BookingDetailsServices

---
+++ src/components/calendarManagement/pendingBookings/AntdCropImgUpload.jsx
@@ -0,0 +1,86 @@
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+
+import React, { useState } from "react";
+import { Upload } from "antd";
+import ImgCrop from "antd-img-crop";
+
+const AntdCropImgUpload = () => {
+  const [fileList, setFileList] = useState([
+    // {
+    //   uid: "-1",
+    //   name: "image.png",
+    //   status: "done",
+    //   url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    // },
+  ]);
+
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
+  const onPreview = (file) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+      let src = file.url;
+      if (!src) {
+        src = yield new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file.originFileObj);
+          reader.onload = () => resolve(reader.result);
+        });
+      }
+      const image = new Image();
+      image.src = src;
+      const imgWindow = window.open(src);
+      imgWindow === null || imgWindow === void 0
+        ? void 0
+        : imgWindow.document.write(image.outerHTML);
+    });
+    
+  return (
+    <ImgCrop rotationSlider>
+      <Upload
+        action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+        listType="picture-card"
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+      >
+        {fileList.length < 5 && "+ Upload"}
+      </Upload>
+    </ImgCrop>
+  );
+};
+
+export default AntdCropImgUpload;

e8a81083d0d897731f628917be15f3d3cfb13379
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-20 03:59:35 +0600

    responsiveness of  booking flow by service provider client info checkout summary

--- src/pages/calenderManagement/addBookingByProvider/ClientInfoFormPage.jsx
+++ src/pages/calenderManagement/addBookingByProvider/ClientInfoFormPage.jsx
@@ -3,6 +3,11 @@ import { useNavigate } from "react-router";
 import ProviderCheckoutCard from "../../../components/addBookingByProvider/ProviderCheckoutCard";
 import Breadcrumb from "../../../components/client/Breadcrumb";
 import { getBreadcrumbs } from "../../../lib/staticData";
+import { FaStar } from "react-icons/fa";
+import { ChevronUp } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { PiFireLight, PiInfo } from "react-icons/pi";
+import { useState } from "react";
 
 const options = [
   { value: "Burns Bay Road" },
@@ -16,11 +21,19 @@ const businessStaticData = {
   rating: 4.8,
   reviewCount: "12.5K reviews",
   address: "15 Rothschild Boulevard, Tel Aviv-Yafo, Israel",
+  subtotal: 20.0,
+  vatIncluded: true,
+  discountPercentage: 10.0,
+  discountAmount: 60.0,
+  total: 90.0,
+  paymentInstruction: "You will pay at the appointment location",
 };
 
 const ClientInfoFormPage = () => {
   const navigate = useNavigate();
   const [form] = Form.useForm();
+
+  const [showDetails, setShowDetails] = useState(false);
 
   const handleBookNow = async () => {
     try {
@@ -33,41 +46,43 @@ const ClientInfoFormPage = () => {
   };
 
   return (
-    <section>
-      <Breadcrumb
-        breadcrumbs={getBreadcrumbs(0, 3, [
-          {
-            name: "Client information",
-            link: "/dashboard/add-booking-by-provider",
-          },
-          {
-            name: "Select Services",
-            link: "/dashboard/add-booking-by-provider/select-services",
-          },
-          {
-            name: "Select staff",
-            link: "/dashboard/add-booking-by-provider/select-staff",
-          },
-          {
-            name: "Select Time",
-            link: "/dashboard/add-booking-by-provider/select-time",
-          },
-          {
-            name: "Confirm",
-            link: "/dashboard/add-booking-by-provider/confirm",
-          },
-        ])}
-      />
-      <div className="flex flex-col md:flex-row justify-between gap-4 items-start">
-        <div className="p-5 rounded-xl bg-[#FFFFFF] shadow-md space-y-3 flex-1 w-full md:w-auto">
+    <section className="">
+      <div className="max-md:px-3">
+        <Breadcrumb
+          breadcrumbs={getBreadcrumbs(0, 3, [
+            {
+              name: "Client information",
+              link: "/dashboard/add-booking-by-provider",
+            },
+            {
+              name: "Select Services",
+              link: "/dashboard/add-booking-by-provider/select-services",
+            },
+            {
+              name: "Select staff",
+              link: "/dashboard/add-booking-by-provider/select-staff",
+            },
+            {
+              name: "Select Time",
+              link: "/dashboard/add-booking-by-provider/select-time",
+            },
+            {
+              name: "Confirm",
+              link: "/dashboard/add-booking-by-provider/confirm",
+            },
+          ])}
+        />
+      </div>
+      <div className="flex flex-col md:flex-row justify-between gap-4 items-start max-md:px-3">
+        <div className="p-5 rounded-xl bg-[#FFFFFF] shadow space-y-3 flex-1 w-full md:w-auto">
           <h3 className="text-[#262626] text-[16px] font-semibold">
             Client Information
           </h3>
 
           <Form
-            form={form} 
-            layout="vertical" 
-            initialValues={{ sex: "male" }} 
+            form={form}
+            layout="vertical"
+            initialValues={{ sex: "male" }}
             requiredMark={false}
           >
             <div>
@@ -79,7 +94,7 @@ const ClientInfoFormPage = () => {
                         Name <sup className="text-red-600">*</sup>
                       </span>
                     }
-                    name="clientName" 
+                    name="clientName"
                     rules={[
                       {
                         required: true,
@@ -110,14 +125,14 @@ const ClientInfoFormPage = () => {
                         Phone Number <sup className="text-red-600">*</sup>
                       </span>
                     }
-                    name="phoneNumber" 
+                    name="phoneNumber"
                     rules={[
                       {
                         required: true,
                         message: "Please input the phone number!",
                       },
                       {
-                        pattern: /^[0-9]{10,15}$/, 
+                        pattern: /^[0-9]{10,15}$/,
                         message: "Please enter a valid phone number!",
                       },
                     ]}
@@ -125,7 +140,7 @@ const ClientInfoFormPage = () => {
                     <Input
                       placeholder="Your Phone Number"
                       size="large"
-                      type="tel" 
+                      type="tel"
                     />
                   </Form.Item>
                 </div>
@@ -137,7 +152,7 @@ const ClientInfoFormPage = () => {
                       Sex <sup className="text-red-600">*</sup>
                     </span>
                   }
-                  name="sex" 
+                  name="sex"
                   rules={[
                     {
                       required: true,
@@ -158,12 +173,140 @@ const ClientInfoFormPage = () => {
           </Form>
         </div>
 
-        <div className="w-full md:w-auto mt-4 md:mt-0">
+        <div className="w-full md:w-auto mt-4 md:mt-0 max-md:hidden">
           <ProviderCheckoutCard
             businessData={businessStaticData}
             handleBookNow={handleBookNow}
           />
         </div>
+      </div>
+
+      {/* Mobile View */}
+      <div className="md:hidden bg-white p-4 shadow fixed bottom-0 w-full z-50">
+        <div className="flex items-end justify-between">
+          <div>
+            <h3 className="self-stretch text-Boulder-950 text-lg font-semibold leading-relaxed">
+              {businessStaticData.studioName}
+            </h3>
+
+            {(businessStaticData.rating || businessStaticData.reviewCount) && (
+              <div className="flex gap-1 items-center">
+                {businessStaticData.rating && (
+                  <FaStar className="text-[#FFD056]" />
+                )}
+                {businessStaticData.rating && (
+                  <p className="text-black text-sm font-normal leading-tight">
+                    {businessStaticData.rating}
+                  </p>
+                )}
+                {businessStaticData.reviewCount && (
+                  <p className="text-description text-sm font-normal underline leading-tight">
+                    ({businessStaticData.reviewCount})
+                  </p>
+                )}
+              </div>
+            )}
+          </div>
+
+          <div className="self-end">
+            <Button
+              type="text"
+              className="w-full"
+              onClick={() => setShowDetails(!showDetails)}
+            >
+              See detail{" "}
+              {showDetails ? (
+                <ChevronUp size={14} />
+              ) : (
+                <ChevronDown size={14} />
+              )}
+            </Button>
+          </div>
+        </div>
+        <div className="border-b border-b-gray-200 my-3" />
+
+        {/* Price details */}
+        <div className="flex flex-col gap-[12px] w-full justify-center items-start">
+          {showDetails && (
+            <>
+              {(!!businessStaticData.subtotal ||
+                businessStaticData.vatIncluded) && (
+                <div className="flex justify-between items-start w-full mb-2">
+                  <p className="self-stretch text-description text-sm font-normal   leading-tight flex items-center gap-1">
+                    Subtotal{" "}
+                    {businessStaticData.subtotal && (
+                      <PiInfo className="text-gray-500" />
+                    )}
+                  </p>
+                  <div>
+                    {businessStaticData.subtotal && (
+                      <p className="text-right text-black text-sm font-normal leading-tight">
+                        {businessStaticData.subtotal}
+                      </p>
+                    )}
+                    {businessStaticData.vatIncluded && (
+                      <p className=" text-description text-xs font-normal text-right  leading-none">
+                        {businessStaticData.vatIncluded}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              )}
+              {(businessStaticData.discountPercentage ||
+                businessStaticData.discountAmount) && (
+                <div className="flex justify-between items-start w-full">
+                  <p className="self-stretch text-description text-sm font-normal   leading-tight flex items-center gap-1">
+                    Discount
+                  </p>
+                  <div className="flex gap-1 items-center">
+                    {businessStaticData.discountPercentage && (
+                      <p className="text-violet-500 text-xs font-medium   leading-none px-2 py-1 flex bg-[#ecebfc] w-max rounded items-center gap-1">
+                        <PiFireLight /> {businessStaticData.discountPercentage}
+                      </p>
+                    )}
+                    {businessStaticData.discountAmount && (
+                      <p className="text-right text-red-500 text-sm font-normal   leading-tight">
+                        {businessStaticData.discountAmount}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              )}
+              {/* Separator Line for Total - Renders only if discount or subtotal is present AND total is present */}
+              {(businessStaticData.subtotal ||
+                businessStaticData.discountAmount) &&
+                businessStaticData.total && (
+                  <div className="h-[1px] w-full border-t border-[#E9EAEC]"></div>
+                )}
+            </>
+          )}
+
+          {businessStaticData.total && (
+            <div className="flex justify-between items-start w-full">
+              <p className="w-full max-w-48 text-description text-sm font-normal   leading-tight">
+                Total
+              </p>
+              <p className="text-right text-violet-500 text-lg font-semibold leading-relaxed">
+                {businessStaticData.total}
+              </p>
+            </div>
+          )}
+        </div>
+
+        <Button
+          color="default"
+          variant="solid"
+          className="w-full my-2"
+          disabled
+        >
+          Continue
+        </Button>
+
+        {businessStaticData.paymentInstruction && (
+          <p className="self-stretch text-center text-description text-xs font-normal   leading-none">
+            {businessStaticData.paymentInstruction}
+          </p>
+        )}
       </div>
     </section>
   );

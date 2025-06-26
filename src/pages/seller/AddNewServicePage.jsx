5dd9173072acf41c231f4e5222fdd8f985fda50f
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-06-27 03:21:17 +0600

    refactor: update AddNewServicePage structure and routing; enhance user experience with conditional rendering

--- src/pages/seller/AddNewServicePage.jsx
+++ src/pages/seller/AddNewServicePage.jsx
@@ -2,20 +2,20 @@ import { Button, Space } from "antd";
 import { ArrowLeft } from "lucide-react";
 import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
-import ImageCropModal from "../../components/modal/ImageCropModal";
-import ServicePriceSetting from "../../components/DashboardPageComponents/shared/ServicePriceSetting";
-import ServiceImageUpload from "../../components/DashboardPageComponents/shared/ServiceImageUpload";
+import { Link, useLocation, useNavigate, useSearchParams } from "react-router";
 import ServiceBasicDetails from "../../components/DashboardPageComponents/shared/ServiceBasicDetails";
 import ServiceBeforeAfterImageUpload from "../../components/DashboardPageComponents/shared/ServiceBeforeAfterImageUpload";
-import { use } from "react";
-import { useLocation, useNavigate, useParams, useSearchParams } from "react-router";
+import ServiceImageUpload from "../../components/DashboardPageComponents/shared/ServiceImageUpload";
+import ServicePriceSetting from "../../components/DashboardPageComponents/shared/ServicePriceSetting";
+import ImageCropModal from "../../components/modal/ImageCropModal";
 
-const AddNewServicePage = () => {
+const AddNewServicePage = ({ publishedBtn = false }) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const beforeAfter = location.state?.beforeAfter;
+  const beforeAfter = "Before & After";
+  // const beforeAfter = location.state?.beforeAfter;
   const [searchParams] = useSearchParams();
-  const serviceId = searchParams.get('serviceId');
+  const serviceId = searchParams.get("serviceId");
 
   const [activeKey, setActiveKey] = useState(["1"]);
   const [isStepComplete, setIsStepComplete] = useState(false);
@@ -158,18 +158,21 @@ const AddNewServicePage = () => {
 
   return (
     <div className="w-full p-4 md:p-5">
-      <div className="flex justify-between items-center">
-        <div
-          onClick={() => navigate(-1)}
-          className="flex items-center gap-1.5 cursor-pointer"
-        >
-          <ArrowLeft size={18} />
-          <p className="text-[#242528] text-lg font-semibold ml-1">{serviceId ? "Update" : "New"} Service</p>
+      {publishedBtn && (
+        <div className="flex justify-between items-center">
+          <div
+            onClick={() => navigate(-1)}
+            className="flex items-center gap-1.5 cursor-pointer">
+            <ArrowLeft size={18} />
+            <p className="text-[#242528] text-lg font-semibold ml-1">
+              {serviceId ? "Update" : "New"} Service
+            </p>
+          </div>
+          <Button color="default" variant="solid" disabled>
+            Publish
+          </Button>
         </div>
-        <Button color="default" variant="solid" disabled>
-          Publish
-        </Button>
-      </div>
+      )}
       <div className="mb-20 md:mb-0">
         <Space direction="vertical" className="w-full my-4 space-y-6">
           <ServiceBasicDetails
@@ -247,11 +250,25 @@ const AddNewServicePage = () => {
         onCropFinish={handleCropFinish}
       />
 
+      {!publishedBtn && (
+        <div className="sm:w-auto flex items-center justify-end gap-4 my-6 mx-2 sm:mx-5">
+          <Link to={-1} className="w-full sm:w-auto">
+            <button className="w-full sm:w-auto px-[18px] py-[8px] border font-medium border-[#242528] rounded-lg hover:scale-95 transform transition-all duration-300 ease-in-out hover:bg-[#f3f3f3] hover:shadow-md">
+              Previous
+            </button>
+          </Link>
+          <Link to={"/onboard/service-table"} className="w-full sm:w-auto">
+            <button className="w-full sm:w-auto px-[18px] py-[8px] bg-[#242528] text-[#fff] border font-medium rounded-lg hover:scale-95 transition-all duration-300 ease-in-out hover:bg-[#3a3a3a] hover:shadow-md">
+              Continue
+            </button>
+          </Link>
+        </div>
+      )}
+
       <div className="flex justify-between items-center md:hidden fixed bottom-0 left-0 right-0 bg-white pb-10 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] p-5 z-50">
         <button
           type="button"
-          className=" md:hidden text-white font-semibold bg-gray-900 py-2.5 w-full rounded-lg cursor-pointer hover:scale-95 transform transition-all duration-300 ease-in-out"
-        >
+          className=" md:hidden text-white font-semibold bg-gray-900 py-2.5 w-full rounded-lg cursor-pointer hover:scale-95 transform transition-all duration-300 ease-in-out">
           Publish
         </button>
       </div>

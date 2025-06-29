c788402707e60fbd370a90fb2803b1a61cf148fe
mehedi8430 <mehedialhasan1999@gmail.com>
2025-06-29 23:51:38 +0600

    Refactor SetupLocationServices1 and SetupTeamLocationServices1 components to enhance location management; integrate modals for fixed location and delete location, improve UI with button components, and update state handling for better user experience.

--- src/pages/onboarding/solo/SetupLocationServices1.jsx
+++ src/pages/onboarding/solo/SetupLocationServices1.jsx
@@ -1,11 +1,56 @@
-import { Switch } from "antd";
+import { Button, Switch } from "antd";
 import { Plus } from "lucide-react";
 import { Link } from "react-router";
+import FixedLocationModal from "../../../components/modal/FixedLocationModal";
+import { useState } from "react";
+import LocationList from "../../../components/SettingsPages/LocationList";
+import DeleteLocationModal from "../../../components/modal/DeleteLocationModal";
+import { MobileServiceSetupModal } from "../../../components/SettingsPages/mobileServiceSetup/MobileServiceSetupModal";
+import MobileServiceLocationList from "../../../components/SettingsPages/MobileServiceLocationList";
+import CitySelectionModal from "../../../components/modal/CitySelectionModal";
 
 const SetupLocationServices1 = () => {
+  const [fixedLocationModalOpen, setFixedLocationModalOpen] = useState(false);
+  const [deleteLocationModalOpen, setDeleteLocationModalOpen] = useState(false);
+  const [openCitySelectionModal, setOpenCitySelectionModal] = useState(false);
+  const [mobileModalOpen, setMobileModalOpen] = useState(false);
+
+  const [selectedDistrict, setSelectedDistrict] = useState("");
+  const [selectedCities, setSelectedCities] = useState({});
+  const [mobileServiceEnabled, setMobileServiceEnabled] = useState(false);
+
+  const handleCityChange = (city) => {
+    console.log("city", city);
+    setSelectedCities((prev) => {
+      const district = selectedDistrict;
+
+      const current = prev[district] || [];
+
+      const updated = current.includes(city)
+        ? current.filter((c) => c !== city)
+        : [...current, city];
+
+      return {
+        ...prev,
+        [district]: updated,
+      };
+    });
+  };
+
   const onChange = (checked) => {
     console.log(`switch to ${checked}`);
   };
+
+  const onChanges = (checked) => {
+    console.log(`switch to ${checked}`);
+    if (checked) setMobileModalOpen(true);
+  };
+
+  const handleSwitchChange = (checked) => {
+    setMobileServiceEnabled(checked);
+    onChanges?.(checked);
+  };
+
   return (
     <div className="px-2 py-[20px] lg:px-[20px] lg:py-[30px] xl:p-[40px]">
       <p className="text-[#866BE7] mb-2 font-medium">Step 2 of 3</p>
@@ -28,26 +73,32 @@ const SetupLocationServices1 = () => {
             </p>
           </div>
 
-          <div className="flex items-center gap-2 text-[#744CDB] text-sm sm:text-base font-semibold hover:underline mt-3 xl:mt-0 whitespace-nowrap w-fit">
-            <Plus /> Add Location
+          <div className="hidden md:block">
+            <Button
+              onClick={() => setFixedLocationModalOpen(true)}
+              className="!text-primary01 !border-primary01 "
+            >
+              <Plus size={20} /> Add Location
+            </Button>
           </div>
         </div>
         <hr className="my-6 text-[#F6F6F6]" />
 
-        <div className="flex items-start gap-5">
-          <div>
-            <Switch defaultChecked onChange={onChange} />
-          </div>
-          <div className="flex flex-col justify-start">
-            <h4 className="text-[#242528] font-semibold text-base md:text-lg">
-              Mobile Service
-            </h4>
-            <p className="text-[#797979] my-2 text-sm">
-              Your mobile service is currently inactive. Please enable it to
-              offer on-location services to your clients.
-            </p>
-          </div>
-        </div>
+        {/* location List */}
+        <LocationList
+          locationData={locationData}
+          setFixedLocationModalOpen={setFixedLocationModalOpen}
+          setDeleteLocationModalOpen={setDeleteLocationModalOpen}
+        />
+
+        <hr className="my-6 text-[#F6F6F6]" />
+
+        {/* mobile service location list */}
+        <MobileServiceLocationList
+          setMobileModalOpen={setMobileModalOpen}
+          mobileServiceEnabled={mobileServiceEnabled}
+          handleSwitchChange={handleSwitchChange}
+        />
       </div>
 
       <div className="sm:w-auto flex items-center justify-end gap-4 my-6 mx-2 sm:mx-5">
@@ -57,14 +108,72 @@ const SetupLocationServices1 = () => {
           </button>
         </Link>
 
-        <Link to={"/onboard/setup-services2"} className="w-full sm:w-auto">
-          <button className="w-full sm:w-auto px-[18px] py-[8px] font-medium rounded-lg hover:scale-95 transform transition-all duration-300 ease-in-out  hover:shadow-md text-[#82868E] bg-[#E5E7E8] hover:bg-[#cccfd1]">
+        <Link to={"/onboard/service"} className="w-full sm:w-auto">
+          <Button color="default" variant="solid" size="large">
             Continue
-          </button>
+          </Button>
         </Link>
       </div>
+
+      {/* Fixed Location Setup Modal */}
+      {fixedLocationModalOpen && (
+        <FixedLocationModal
+          isOpen={fixedLocationModalOpen}
+          onClose={() => setFixedLocationModalOpen(false)}
+          onSave={() => setFixedLocationModalOpen(false)}
+        />
+      )}
+
+      {/* Mobile Service Setup Modal */}
+      {mobileModalOpen && (
+        <MobileServiceSetupModal
+          setMobileModalOpen={setMobileModalOpen}
+          isOpen={mobileModalOpen}
+          onClose={() => setMobileModalOpen(false)}
+          selectedCities={selectedCities}
+          setSelectedDistrict={setSelectedDistrict}
+          setOpenCitySelectionModal={setOpenCitySelectionModal}
+          handleCityChange={handleCityChange}
+        />
+      )}
+
+      {openCitySelectionModal && (
+        <CitySelectionModal
+          openCitySelectionModal={openCitySelectionModal}
+          onClose={() => setOpenCitySelectionModal(false)}
+          selectedDistrict={selectedDistrict}
+          handleCityChange={handleCityChange}
+          selectedCities={selectedCities}
+        />
+      )}
+
+      {/* delete location modal */}
+      {deleteLocationModalOpen && (
+        <DeleteLocationModal
+          isOpen={deleteLocationModalOpen}
+          onClose={() => setDeleteLocationModalOpen(false)}
+          setDeleteLocationModalOpen={setDeleteLocationModalOpen}
+          onDeleteConfirm={() => setDeleteLocationModalOpen(false)}
+          title="This location"
+        />
+      )}
     </div>
   );
 };
 
 export default SetupLocationServices1;
+
+const locationData = [
+  {
+    id: 1,
+    name: "TCL Beauty Studio 01",
+    address: "15 Rothschild Boulevard, Tel Aviv-Yafo, Israel",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "TCL Beauty Studio 02",
+    address: "15 Rothschild Boulevard, Tel Aviv-Yafo, Israel",
+    isActive: false,
+  },
+];
